d2ae7aaac5ac6b8c5e5768a63b72810b50d6256d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-15 19:56:29 +0600

    register page design is done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 rounded-none shadow-2xl">
                 <form className="card-body">
-                    <h2 className="text-2xl font-semibold text-center">Login Your Account</h2>
+                    <h2 className="text-2xl font-semibold text-center">Register Your Account</h2>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo-Url</span>
+                        </label>
+                        <input type="text" placeholder="Photo-Url" className="input input-bordered" required />
+                    </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -24,11 +36,11 @@ const Login = () => {
                     <div className="form-control mt-6">
                         <button className="btn btn-neutral rounded-none">Login</button>
                     </div>
-                    <h3 >Do not have an account? <Link className="text-red-500 font-semibold hover:underline" to="/auth/register">Register</Link ></h3>
+                    <h3 >Already have an account? <Link className="text-red-500 font-semibold hover:underline" to="/auth/login">Login</Link ></h3>
                 </form>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;
