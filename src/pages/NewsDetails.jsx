b1bef2a344fe0054f151e8adebd70bb22b229eba
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-17 15:26:38 +0600

    navigate to news category

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Header from "../components/Header";
 import RightNav from "../components/layout-component/RightNav";
 
@@ -11,20 +11,22 @@ const NewsDetails = () => {
             <header>
                 <Header></Header>
             </header>
-            <main className="grid grid-cols-12 w-11/12 mx-auto">
+            <main className="grid grid-cols-12 w-11/12 gap-6 mx-auto">
                 <section className="col-span-9">
-                    <div className="card bg-base-100 w-96 shadow-xl">
-                        <figure className="px-10 pt-10">
+                    <div className="card bg-base-100 shadow-xl">
+                        <figure className="p-5">
                             <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                src={news.thumbnail_url
+                                }
                                 alt="Shoes"
-                                className="rounded-xl" />
+                                className="rounded-xl w-full h-96" />
                         </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-body items-center ">
+                            <h2 className="card-title">{news.title}</h2>
+                            <p>{news.details}</p>
                             <div className="card-actions">
-                                <button className="btn btn-primary">Buy Now</button>
+                                <Link to={`/category/${news.category_id
+                                    }`} className="btn btn-primary ">Buy Now</Link>
                             </div>
                         </div>
                     </div>
